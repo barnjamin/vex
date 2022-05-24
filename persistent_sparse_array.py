8a5508df184b87dc1e7a91856344ce7082e07945
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-05-24 12:58:14 -0400

    guh

--- persistent_sparse_array.py
+++ persistent_sparse_array.py
@@ -4,47 +4,45 @@ from models import NamedTuple
 
 kb = int(2**10)
 MAX_BOX_SIZE = 16 * kb
-
-BOX_MBR = 2500
+BOX_MBR   = 2500
 BOX_BYTES = 400
 
 
-# General strategy:
-#   when num slots is avail is low, include extra page reference
-#   in transaction
-
 PageIndicies = abi.StaticArray[abi.Uint8, Literal[32]]
 
-# Pages represents up to N pages of orders,
-# each page has an address (32 byte array)
-# each index for each page has an associated bit
-# in the Address byte array
-# is stored at a uint8 index per page
-
-# box key prefix
-# track slots open
-# if no slots open,
-# manage adding pages, (need txn to include it)
-# todo: figure out who to charge?
-# assign slot to write
-# ABI r/w codec
-# static ABI Type _only_, so we can track offsets
-
-class Doofus:
+class SparseArray:
 
     def __init__(self, key: str, t: NamedTuple, size: int):
         self.key = key
         self.t = t
-        self.encoded_size = abi.sizeof(t.get_type())
+        self.element_size = abi.sizeof(t.get_type())
         self.pages = abi.make(abi.DynamicArray[PageIndicies])
 
     def initialize(self) -> Expr:
+        """initialize creates the box to hold pages of array elements"""
         return BoxCreate(Bytes(f"{self.key}-pages"), Int(MAX_BOX_SIZE))
 
+    def alloc(self) -> Expr:
+        """Create a new Box to use for storage"""
+        return Seq(
+            self.pages.decode(self.get_page_indicies()),
+            (page_name := ScratchVar()).store(
+                self.page_name(Suffix(Itob(self.pages.length()), Int(7))),
+            ),
+            BoxCreate(
+                page_name.load(),
+                Int(MAX_BOX_SIZE),
+            ),
+            page_name.load()
+        )
+
     def dealloc(self, page: Expr) -> Expr:
+        """Deletes Box from storage, used when we dont need it anymore"""
         return BoxDelete(self.page_name(page))
 
+
     def get_page_indicies(self) -> Expr:
+        """Read in all the page indicies"""
         return BoxExtract(
             Bytes(f"{self.key}-pages"),
             Int(0),
@@ -52,21 +50,16 @@ class Doofus:
         )
 
     def write_page_indicies(self, page: Expr) -> Expr:
+        """Write out page indicies"""
         return BoxReplace(Bytes(f"{self.key}-pages"), Int(0), page)
 
     def page_name(self, page: Expr) -> Expr:
+        """Page name is key + page"""
         return Concat(Bytes(self.key), page)
 
-    def alloc(self) -> Expr:
-        return Seq(
-            self.pages.decode(self.get_page_indicies()),
-            BoxCreate(
-                self.page_name(Suffix(Itob(self.pages.length()), Int(7))),
-                Int(MAX_BOX_SIZE),
-            ),
-        )
 
     def reserve_slot(self) -> Expr:
+        """ Find first open slot """
         return Seq(
             (index := ScratchVar()).store(Int(0)),
             (page := ScratchVar()).store(Int(0)),
@@ -90,6 +83,7 @@ class Doofus:
                     ),
                 )
             ),
+
             # If we need to allocate a new page, do it
             If(
                 And(page.load() == Int(0), index.load() == Int(0)),
@@ -98,30 +92,37 @@ class Doofus:
                     page.store(self.pages.length()),
                 )
             ),
+
+            # Return slot = [page, index]
             Concat(
                 Suffix(Itob(page.load()), Int(7)), 
                 Suffix(Itob(index.load()), Int(7))
             ),
         )
 
-    def write(self, slot: abi.Uint16, t: "NamedTuple") -> Expr:
+    def put(self, slot: abi.Uint16, t: "NamedTuple") -> Expr:
+        """ Write T into slot """
         return Seq(
             (page := abi.Uint8()).decode(Extract(slot.encode(), Int(0), Int(1))),
             (idx := abi.Uint8()).decode(Extract(slot.encode(), Int(1), Int(1))),
             BoxReplace(
                 self.page_name(page.encode()),
-                Int(self.encoded_size) * idx.get(),
+                Int(self.element_size) * idx.get(),
                 t.encode(),
             ),
         )
 
-    def read(self, slot: abi.Uint16)->Expr:
+    def get(self, slot: abi.Uint16)->Expr:
+        """ Get T from slot """
         return Seq(
             (page := abi.Uint8()).decode(Extract(slot.encode(), Int(0), Int(1))),
             (idx := abi.Uint8()).decode(Extract(slot.encode(), Int(1), Int(1))),
             BoxExtract(
                 self.page_name(page.encode()),
-                Int(self.encoded_size) * idx.get(),
-                Int(self.encoded_size) * (idx.get()+Int(1))
+                Int(self.element_size) * idx.get(),
+                Int(self.element_size) * (idx.get()+Int(1))
             ),
         )
+
+    def delete(self, slot: abi.Uint16)->Expr:
+        pass
--- storage.md
+++ storage.md
@@ -1,33 +1,29 @@
-Sparse Array Storage:
+Sparse Array Storage T[]:
 
     T: Known Type
     N: Known size of T  `abi.size_of(T)`
     MaxElems: Max elements per Box `floor(floor(MAX_BOX/N)/8)*8`. So we can use a `byte[]` for indicies 
 
-    Page: Box holding data elements
+    Page: Box holding data elements `T[MaxElems]`
     Index: reletive position in each page
     Slot: Absolute position `[Page, Index]`
 
-    Slots Box: Contains Array of Bitmaps
+    Table Of Contents: Contains Array of Bitmaps
         - Each Bitmap is represents single page of storage 
         - BitMap: bit[MaxElems], 0 for filled, 1 for free (use bitlen to find first open index)
-
-    Page: T[MaxElems] 
 
     put(T):
         slot = Find first open slot (iterate over page bitmaps, bitlen for first index, should prefer lower index boxes && indicies)
         mark_occupied(slot)
-        [page, index] = slot
-        BoxReplace(page, index*N, T.encode()) into page/index
-        return slot
+        update(slot, T)
+
+    get(slot):
+        [page, index] = slot 
+        T.decode(BoxExtract(page, index*N, (index+1)*N))
 
     update(slot, T):
         [page, index] = slot
         BoxReplace(page, index*N, T.encode())
-
-    read(slot):
-        [page, index] = slot 
-        T.decode(BoxExtract(page, index*N, (index+1)*N))
 
     delete(slot):
         mark_unoccupied(slot)
@@ -35,40 +31,42 @@ Sparse Array Storage:
 
 Tracking interesting slots is up to the application
 
+Alloc/Dealloc should be private methods that allow for new Box creation/deletion as needed 
+
 ex:
     FIFO queue as double linked list to allow deletion, head/tail and higher/lower pointers are slots to elements
 
 
+# Oder Book
 
+Each side of the book contains some N orders, each filled according to price/time
 
+BidSide = [
+    10.01: [{}, {}, {}],
+    10.02: [],
+    10.03: [{}, {}, {}, {}, {}, {}],
+    ...
+]
 
-############################
+Orders are filled by price from mid price out. Within each price, orders are filled from first order placed to last as in a Priority Queue. 
 
+Each order is appended to the Priority Queue that represents the list of orders at that price by updating pointers in the previous/next entries to reference the new order.
 
-Is it reasonable to set a max number of elements per Price Queue ? 
-Is it reasonable to set a min size of order? max decimals?
+Each order is stored to the first open slot, which may be _any_ box with free space.
 
+An Order can be modified in place by having its size changed.
+An Order can be filled or cancelled by marking the slot as empty and updating pointers in adjacent entries.
 
-Per Box 2500
-Per Byte 400
+## Challange
 
+Because we may specify a max 8 Boxes per app call and all Boxes to be accessed must be specified from off chain, we need to 
+be able to determine _which_ boxes should be requested in the case of an incoming order.
 
-Limits
-    Max 8 Boxes per app call (?)
-    All Boxes to be accessed must be specified from off chain txn 
+Ideally, the entire contiguous list of orders to fill are accessible in a single transaction.
 
+If we naively place orders in any available slots, a single price's priority queue may have its orders scattered across N>8 Boxes.
 
+    At 64b/order and 16kb/box we can store 256 orders per box. With 8 Boxes of access we may access up to 2048 orders in a single application call. 
+    But out of those 2048, there is no guarantee that these orders are the ones that _should_ be filled next according to price/time priority.
 
-Boxes for:
-    Order Queue Per Side
-        head/tail * 4000
-        note: 
-            at 2 decimals, this is 40 whole units from mid
-            at 3 decimals, this is 4 whole units
-            at 4 decimals, this is 0.4
-            ...
-            Adjust min order size to keep decimals in a reasonable ratio
-
-
-Each OrderQueue Page:
-    order[256]
+How can we keep both priority queue elements and sequential priority queues in order on disk?
